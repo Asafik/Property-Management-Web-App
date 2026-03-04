a3491f8e3c148ef528c8d9f060ca38300f971154
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2026-03-04 14:21:43 +0700

    P

--- database/migrations/0001_01_01_000008_create_rab_deadlines_table.php
+++ database/migrations/0001_01_01_000008_create_rab_deadlines_table.php
@@ -10,9 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        
-    }
+{
+    Schema::create('rab_deadlines', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('development_progress_id')
+            ->constrained('development_progress')
+            ->onDelete('cascade');
+
+        $table->date('target_mulai');
+        $table->date('target_selesai');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
