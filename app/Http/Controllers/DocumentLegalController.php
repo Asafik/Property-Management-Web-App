8b6d24d3881f24909751fbf3018611bffaabbb82
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2026-03-28 09:58:20 +0700

    fixed document cash keras done

--- app/Http/Controllers/DocumentLegalController.php
+++ app/Http/Controllers/DocumentLegalController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Booking;
-use App\Models\Document; // <- jangan lupa import
+use App\Models\Document;
+use App\Models\DocumentUpload;
 
 class DocumentLegalController extends Controller
 {
@@ -14,7 +15,43 @@ class DocumentLegalController extends Controller
 
         // Ambil master dokumen
         $documents = Document::orderBy('id')->get();
-
-        return view('marketing.cash_dokument_legal', compact('booking', 'documents'));
+        $uploads = DocumentUpload::where('booking_id', $id)->get()->keyBy('document_id');
+        return view('marketing.cash_dokument_legal', compact('booking', 'documents','uploads'));
     }
+
+        public function store(Request $request, $bookingId)
+        {
+        
+            $request->validate([
+                'document_*' => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:5120',
+            ]);
+
+            foreach ($request->allFiles() as $key => $file) {
+
+                if ($file) {
+
+                    
+                    $documentId = str_replace('document_', '', $key);
+
+                    $path = $file->store('document_uploads', 'public');
+
+                
+                    DocumentUpload::updateOrCreate(
+                        [
+                            'booking_id'  => $bookingId,
+                            'document_id' => $documentId,
+                        ],
+                        [
+                            'file_name' => $file->getClientOriginalName(),
+                            'file_path' => $path,
+                        ]
+                    );
+                }
+            }
+                    Booking::where('id', $bookingId)->update([
+                'status_legal' => 'done'
+            ]);
+
+            return redirect()->back()->with('success', 'Dokumen berhasil diupload');
+        }
 }
